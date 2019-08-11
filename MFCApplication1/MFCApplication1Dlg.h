1564d51f4d5db0f5d516b3be421a2c6595552840
taehyeong77@naver.com <taehyeong77@naver.com>
2019-08-11 23:34:51 +0900

    commit

---
+++ MFCApplication1/MFCApplication1Dlg.h
@@ -0,0 +1,54 @@
+﻿
+// MFCApplication1Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+#include "MyListBox.h"
+
+#define MAX_ITEM_COUNT	8
+
+// CMFCApplication1Dlg 대화 상자
+class CMFCApplication1Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCApplication1Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+private:
+	//CCheckListBox m_item_list;
+	MyListBox m_item_list;
+public:
+	afx_msg void OnEnChangeTotalPriceEdit();
+	afx_msg void OnLbnSelchangeItemList();
+	//CListBox m_count_list;
+	void CalcTotalPrice();
+	//void ChangeText(CListBox * ap_list_box, int a_index, const wchar_t *ap_string);
+
+	CRect m_spin_rect;
+	CSpinButtonCtrl m_count_spin;
+	afx_msg void OnDeltaposCountSpin(NMHDR *pNMHDR, LRESULT *pResult);
+	CFont m_my_font;
+
+	
+	afx_msg void OnDestroy();
+};
---
+++ MFCApplication1/MyListBox.cpp
@@ -0,0 +1,64 @@
+﻿// MyListBox.cpp: 구현 파일
+//
+
+#include "stdafx.h"
+#include "MFCApplication1.h"
+#include "MyListBox.h"
+
+
+// MyListBox
+
+IMPLEMENT_DYNAMIC(MyListBox, CCheckListBox)
+
+MyListBox::MyListBox()
+{
+
+}
+
+MyListBox::~MyListBox()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(MyListBox, CCheckListBox)
+	ON_WM_KEYDOWN()
+END_MESSAGE_MAP()
+
+
+
+// MyListBox 메시지 처리기
+
+
+
+
+void MyListBox::DrawItem(LPDRAWITEMSTRUCT lpds)
+{
+	CCheckListBox::DrawItem(lpds);
+	// 그리고 싶은 내용
+	CDC * p_dc = CDC::FromHandle(lpds->hDC);
+
+	CString str;
+	//ItemInfo * p = (ItemInfo*)GetItemDataPtr(lpds->itemID);
+	ItemInfo * p = (ItemInfo*)lpds->itemData;
+	
+	if (p->count > 1) {
+		str.Format(L"%d개(%d)", p->count, p->price);
+	}
+	else {
+		str.Format(L"%d개", p->count);
+	}
+	
+	p_dc->SetBkMode(TRANSPARENT);
+	if(lpds->itemState & ODS_SELECTED)p_dc->SetTextColor(RGB(255, 255, 0));
+	
+	p_dc->DrawText(str, &lpds->rcItem, DT_RIGHT|DT_VCENTER|DT_SINGLELINE);
+
+}
+
+
+void MyListBox::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+
+	CCheckListBox::OnKeyDown(nChar, nRepCnt, nFlags);
+	GetParent()->SendMessage(WM_COMMAND,MAKEWPARAM(GetDlgCtrlID(), LBN_SELCHANGE) , (LPARAM)m_hWnd);
+}
